c0afe4905b23a6ee201be4170b212efc6a46bf21
Ancalima <raul.vent.bg@gmail.com>
2015-07-15 10:58:03 +0200

    Update arp_3750.py

--- arp_3750.py
+++ arp_3750.py
@@ -8,6 +8,10 @@ if len(sys.argv) == 1:
 # Inserire il nome del file da cercare
 filename = sys.argv[1]
 #filename = 'arp_3750.txt'
+device = str(filename)
+pattern_device = r'([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})'
+match_pattern = re.search(pattern_device, device)
+device_name = '|' + match_pattern.group(0)
 # Inserire il nome del file di destinazione
 filename2 = 'arp_3750.txt'
 #Regex per trovare gli spazi e i punti
@@ -37,7 +41,7 @@ for line in lines:
         
         new_line = re.sub(spaces, '|', new_line)
         #Appende la nuova line nell'array new_file
-        print new_line
+        print new_line + device_name
         #new_file.append(new_line)
 
 #Scrive un nuovo file con il file name inserito all'inizio dello script
